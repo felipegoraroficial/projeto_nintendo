32693309fd0628172b5201595edcab8670de8120
Felipe Pegoraro <felipegoraro@outlook.com.br>
2024-12-19 17:41:49 +0000

    subindo processo dbt em fase de teste

--- dbt/union_tables.sql
+++
@@ -1,9 +0,0 @@
-%sql
-SELECT *
-FROM (
-    SELECT *, 'Magalu' AS tabela_origem
-    FROM {{source('nintendo_projeto_dev','mkt-magalu')}}
-    UNION ALL
-    SELECT *, 'Mercado Livre' AS tabela_origem
-    FROM {{source('nintendo_projeto_dev','mkt-mercadolivre')}}
-) AS big_table
---
+++ models/union_tables.sql
@@ -0,0 +1,10 @@
+%s ql
+select *
+from
+    (
+        select *, 'Magalu' as tabela_origem
+        from {{ source("nintendo_projeto_dev", "mkt-magalu") }}
+        union all
+        select *, 'Mercado Livre' as tabela_origem
+        from {{ source("nintendo_projeto_dev", "mkt-mercadolivre") }}
+    ) as big_table
