69e42ef18b2c0f7d5e8bcc0c1ad707b9cbc39cc9
felipegoraroficial <felipegoraroficial>
2024-12-26 15:12:05 +0000

    teste com parametro

--- nintendo-elt-process/teste.py
+++ nintendo-elt-process/teste.py
@@ -1,26 +1,3 @@
 # Databricks notebook source
-import os
-import json
-
-# COMMAND ----------
-
-# Obtém o caminho do diretório atual
-current_dir = os.getcwd()
-
-# COMMAND ----------
-
-# Ajusta o caminho do diretório para os primeiros 4 níveis
-current_dir = '/'.join(current_dir.split('/')[:4])
-
-# COMMAND ----------
-
-print(current_dir)
-
-# COMMAND ----------
-
-# Define o caminho do arquivo de configuração
-config_path = f"{current_dir}/projeto_nintendo/config.json"
-
-# COMMAND ----------
-
-print(config_path)
+env = dbutils.widgets.get('env')
+print(env)
