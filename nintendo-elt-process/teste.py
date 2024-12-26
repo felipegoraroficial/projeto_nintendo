674c1756cfe32055f9c7144d63e41138f1960bcb
felipegoraroficial <felipegoraroficial>
2024-12-26 14:46:10 +0000

    teste no workflow com git

--- nintendo-elt-process/teste.py
+++ nintendo-elt-process/teste.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
 import os
+import json
 
 # COMMAND ----------
 
@@ -8,4 +9,31 @@ current_dir = os.getcwd()
 
 # COMMAND ----------
 
+# Ajusta o caminho do diretório para os primeiros 4 níveis
+current_dir = '/'.join(current_dir.split('/')[:4])
+
+# COMMAND ----------
+
 current_dir
+
+# COMMAND ----------
+
+# Define o caminho do arquivo de configuração
+config_path = f"{current_dir}/projeto_nintendo/config.json"
+
+# COMMAND ----------
+
+config_path
+
+# COMMAND ----------
+
+# Abre o arquivo de configuração e carrega seu conteúdo em um dicionário
+with open(config_path, "r") as f:
+    config = json.load(f)
+
+# Obtém o valor da chave "env" do dicionário de configuração
+env = config["env"]
+
+# COMMAND ----------
+
+env
