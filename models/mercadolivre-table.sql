85158a389f786fe9f89d8fa28f3d57f133e13929
Felipe Pegoraro <felipegoraro@outlook.com.br>
2025-01-08 02:02:03 +0000

    normalizando tabelas com dbt

--- models/mercadolivre-table.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM {{ source("nintendo_projeto_dev", "nintendo-bigtable") }}
-WHERE file_name LIKE '%mercadolivre%'
-ORDER BY preco_promo ASC
---
+++ models/tabela-detalhes.sql
@@ -0,0 +1,18 @@
+WITH detailstabela AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (ORDER BY link) AS id_temp
+    FROM
+        {{ source("nintendo_projeto_dev", "nintendo-bigtable") }}
+)
+
+SELECT 
+
+    id_temp AS id,
+    link,
+    file_date,
+    status,
+    origem
+
+FROM detailstabela
+ORDER BY id
