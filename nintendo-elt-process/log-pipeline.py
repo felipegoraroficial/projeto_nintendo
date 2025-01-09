458af4050165327718cef96622bf228a5fcf46c5
felipegoraroficial <felipegoraroficial>
2025-01-09 17:22:20 +0000

    gerando tabela de log do processo

--- nintendo-elt-process/log-pipeline.py
+++ nintendo-elt-process/log-pipeline.py
@@ -2,7 +2,7 @@
 import requests
 import json
 import os
-from pyspark.sql.functions import from_unixtime, col, udf
+from pyspark.sql.functions import from_unixtime, col, udf, regexp_extract, round, when, split, to_date, date_format
 from pyspark.sql.types import StructType, StructField, StringType, LongType, BooleanType
 
 # COMMAND ----------
@@ -92,3 +92,59 @@ schema = StructType([
 # COMMAND ----------
 
 df = spark.createDataFrame(logs_list, schema=schema)
+
+# COMMAND ----------
+
+df = df.withColumn('start_time', (col('start_time') / 1000).cast('timestamp'))
+df = df.withColumn('end_time', (col('end_time') / 1000).cast('timestamp'))
+
+# COMMAND ----------
+
+def convert_durantion(ms):
+
+    seconds = ms // 1000
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+
+    return f'{minutes}m:{remaining_seconds}s'
+
+convert_durantion_udf = udf(convert_durantion,StringType())
+
+df = df.withColumn('run_duration', convert_durantion_udf(col('run_duration')))
+
+# COMMAND ----------
+
+df = df.withColumn('state', col('status.termination_details.code')) \
+       .withColumn('message', col('status.termination_details.message')) \
+       .drop('status')
+
+# COMMAND ----------
+
+df = df.withColumn('state', when(col('state') == 'RUN_EXECUTION_ERROR', 'FAILED').otherwise(col('state')))
+
+# COMMAND ----------
+
+df = df.withColumn('start_date', to_date(split(col('start_time'), ' ')[0], 'yyyy-MM-dd')) \
+       .withColumn('start_time', date_format(split(col('start_time'), ' ')[1], 'HH:mm:ss.SSS'))
+
+df = df.withColumn('end_date', to_date(split(col('end_time'), ' ')[0], 'yyyy-MM-dd')) \
+       .withColumn('end_time', date_format(split(col('end_time'), ' ')[1], 'HH:mm:ss.SSS'))
+
+# COMMAND ----------
+
+df = df.select('run_name','run_id','creator_user_name','start_date','start_time','end_date','end_time','run_duration','trigger','run_page_url','state','message')
+
+# COMMAND ----------
+
+df = df.withColumn('run_duration_minutes', regexp_extract(col('run_duration'), '(\d+)m', 1).cast('double')) \
+       .withColumn('run_duration_seconds', regexp_extract(col('run_duration'), '(\d+)s', 1).cast('double')) \
+       .withColumn('run_duration', round(col('run_duration_minutes') + col('run_duration_seconds') / 60, 2)) \
+       .drop('run_duration_minutes', 'run_duration_seconds')
+
+# COMMAND ----------
+
+df = df.orderBy(col('start_date').desc(), col('start_time').desc())
+
+# COMMAND ----------
+
+df.write.mode('overwrite').saveAsTable(f"{env}.`log-table`")
