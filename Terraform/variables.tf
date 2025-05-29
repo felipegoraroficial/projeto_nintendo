eafd90ea86285e1671ab23547c9bde72a953d6a0
Felipe Pegoraro <felipegoraro@outlook.com.br>
2025-05-29 16:21:00 -0300

    alterando cluster no databricks

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "databricks_node_type" {
   type    = string
-  default = "Standard_D3_v2"
+  default = "Standard_DS3_v2"
 }
 
 variable "databricks_spark_version" {
   type    = string
-  default = "10.4.x-scala2.12"
+  default = "16.3.x-scala2.12"
 }
 
 variable "cluster_dev_name" {
